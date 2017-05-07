912542d76f1b5014563948209d289535a900f0ed
Rachel Simone Weil <partytimehexcellent@gmail.com>
2017-05-06 23:31:38 -0500

    Update azure-deploy.cmd

--- azure-deploy.cmd
+++ azure-deploy.cmd
@@ -11,8 +11,6 @@ cd bin
 curl -o hugo0207.zip -L https://github.com/spf13/hugo/releases/download/v0.20.7/hugo_0.20.7_Windows-64bit.zip
 echo Installing Hugo...
 SetLocal DisableDelayedExpansion & d:\7zip\7za x hugo0207.zip
-if !ERRORLEVEL! NEQ 0 goto error
-if not exist hugo.exe goto install
 popd
 
 REM Generate Hugo static site from source on GitHub
@@ -23,18 +21,4 @@ cd ..
 cd ..
 cd repository
 call D:\home\site\deployments\tools\hugo\bin\hugo -d D:\home\site\wwwroot
-goto end
-
-:error
-echo An error has occurred during web site deployment.
-call :exitSetErrorLevel
-call :exitFromFunction 2>nul
-
-:exitSetErrorLevel
-exit /b 1
-
-:exitFromFunction
-()
-
-:end
 echo Hugo build finished successfully.
