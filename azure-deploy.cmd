2245f727dfa33962cd0f31bbadef45df9da857c5
Rachel Simone Weil <partytimehexcellent@gmail.com>
2017-05-06 23:34:53 -0500

    Update azure-deploy.cmd

--- azure-deploy.cmd
+++ azure-deploy.cmd
@@ -11,14 +11,10 @@ cd bin
 curl -o hugo0207.zip -L https://github.com/spf13/hugo/releases/download/v0.20.7/hugo_0.20.7_Windows-64bit.zip
 echo Installing Hugo...
 SetLocal DisableDelayedExpansion & d:\7zip\7za x hugo0207.zip
-popd
+
 
 REM Generate Hugo static site from source on GitHub
 :build
-echo Building Hugo static site...
-cd ..
-cd ..
-cd ..
-cd repository
+popd
 call D:\home\site\deployments\tools\hugo\bin\hugo -d D:\home\site\wwwroot
 echo Hugo build finished successfully.
